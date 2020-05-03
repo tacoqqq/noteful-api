3e6ca16e9a7354c770ce1a681170de84fa93e2f0
daphne Fang <daphneys.fang@gmail.com>
2020-05-03 13:38:40 -0700

    prepare for deploy

--- postgrator-config.js
+++ postgrator-config.js
@@ -4,5 +4,6 @@ module.exports = {
     "migrationDirectory": "migrations",
     "driver": "pg",
     "password": "",
-    "connectionString": (process.env.NODE_ENV === 'test') ? process.env.TEST_DB_URL : process.env.DB_URL,
+    "connectionString": (process.env.NODE_ENV === 'test') ? process.env.TEST_DATABASE_URL : process.env.DATABASE_URL,
+    "ssl": !!process.env.SSL
 }
